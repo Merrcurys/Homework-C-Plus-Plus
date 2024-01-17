92f29a5637bdadeb8b5ecdd17c57a4124f7b2001
Merrcurys <a.sebezhko2017@gmail.com>
2024-01-17 09:21:10 +0300

    add first practic

---
+++ Project_one.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::setlocale(LC_ALL, "Rus");
+    std::cout << "\tЯ не черточка, не минус\n\tЯ прошу, не путай ты нас.\n";
+    std::cout << "\tВсе меня \"тире\" зовут,\n";
+    std::cout << "\tНужноZ\b – буду тут как тут.\n";
+    return 0;
+}
